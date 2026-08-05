e18bd81e8d7d1ddaa901c0314de0f515fb482f62
manpreetee24 <dman.manpreet27@gmail.com>
2026-08-05 18:41:38 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const rollNo = 24001015039 // cant be changed, if tried so generate error
+let name = "Manpreet" // let and var are for variables and their values can be change
+var course = "ENC" // dont use var , scope problem
+cgpa = 8.9 // by default it is a variable and its value can be changed
+let city   //undefined
+console.table([rollNo, name, course, cgpa, city])   //print all values in a table
+
+name = "manprit"
+course = "Btech Enc"
+cgpa = 9.8
+city = "hisar"
+// rollNo = 24001015038
+// console.log(rollNo)
+
+console.table([rollNo, name, course, cgpa, city])
+
+// no need to use semicolon in js
