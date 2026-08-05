fdd8b666102fa2cbc36d4bfd662a2b6d16f3d9b5
manpreetee24 <dman.manpreet27@gmail.com>
2026-08-05 11:35:48 +0000

    datatypes

---
+++ 01_basics/data_types.js
@@ -0,0 +1,15 @@
+let name = "mannu"
+let age = 12
+let isLoggedIn = true
+let state //not any value assigned
+
+console.log(name)
+console.log(age)
+console.log(isLoggedIn)
+
+console.log(typeof "mannu") // string type
+console.log(typeof age)  //number type , for larger values 2 power 53 use bigint
+console.log(typeof isLoggedIn) //boolean type
+console.log(typeof undefined) //undefined is of undefined type
+console.log(typeof state)  //undefined type means not assigned any value
+console.log(typeof null) //null is of object type
