b1a20973e3063f725d044f01d416b0e1a0fec8ee
manpreetee24 <dman.manpreet27@gmail.com>
2026-08-06 18:58:26 +0000

    add strings.js

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -2,19 +2,23 @@ const name = "mannu"
 const dob = 27
 
 console.log(name + dob) //outdated method for string concatenation
+const string1 = "A string primitive";
+const string2 = 'Also a string primitive';
+const string3 = `Yet another string primitive`;
 
 console.log(`hyy my name is ${name} and my date of birth is ${dob} may`) //string interposition - new method
 
-const gameName = new String(`mannu-dhiman-ladsahab`)
-console.log(gameName[0])
+const gameName = new String(`mannu-dhiman-ladsahab`) //with this notation we confirmed that gameName is strictly of string type
+console.log(gameName)
+
+console.log(gameName[0]) //m1 to access individual char in a string
+console.log(gameName.charAt(2))// m2 to access individual char in a string
 
 console.log(gameName.__proto__) //object type
 
 console.log(gameName.length)
 
 console.log(gameName.toUpperCase())
-
-console.log(gameName.charAt(2))
 
 console.log(gameName.indexOf('d'))
 
