068827bb97ce50024fb334a24a2179214159ef60
manpreetee24 <dman.manpreet27@gmail.com>
2026-08-06 10:19:35 +0000

    add operations.js

---
+++ 01_basics/operations.js
@@ -0,0 +1,34 @@
+let value = 3
+console.log(-value)
+
+console.table([2+3, 2-3, 2*3, 2**3, 2/3, 5%2])
+
+let str1, str2, str3
+str1 = str2= str3 = 'mehak'
+console.log(str2)
+
+let string1 = "Mannu"
+let string2 = " dhiman"
+console.log(string1+ string2)
+
+console.log(1 + "2") //12
+console.log("1" + "2") //12
+console.log("1" + 2) //12
+console.log(1 + 2+ "3")//33
+console.log("1" + 2+ 3)//123
+
+
+console.log(3+4 *6 /2) //15
+console.log(-true)//1
+// console.log(true+)  //error
+console.log(+"")
+
+let marks = 25
+marks++
+console.log(marks)//26
+
+let grade = 4.5
+console.log(--grade)
+
+
+//never use such difficult to read and conclude code use brackets to make it more readable and expect results
